adc89e705186970a5da15a85fa5c0be101922417
unknown <mindc1july20@iiht.tech>
2022-09-25 13:20:31 +0530

    First commit

---
+++ src/main/java/com/Springboot/Exception/DoctorsExist.java
@@ -0,0 +1,10 @@
+package com.Springboot.Exception;
+public class DoctorsExist extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public DoctorsExist(String string) {
+	}
+
+}
---
+++ src/main/java/com/Springboot/SweekruthaHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweekruthaHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweekruthaHospitalApplication.class, args);
+	}
+
+}
